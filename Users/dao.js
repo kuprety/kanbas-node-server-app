cb21753830f0ad55001f0b9fbb34db5cb4aa75ee
kritika <103331480+kri4@users.noreply.github.com>
2024-04-16 11:31:29 -0400

    finishing a6

--- Users/dao.js
+++ Users/dao.js
@@ -3,7 +3,10 @@ export const findUserByCredentials = (username, password) => model.findOne({ use
 
 
 
-export const createUser = (user) => model.create(user);
+export const createUser = (user) => {
+  delete user._id
+  return model.create(user);
+}
 
   
 export const findAllUsers = () => model.find();
